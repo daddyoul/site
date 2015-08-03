dbe4b86b77731bc5d68eb848cc6b2e11c30fdb5a
daddyoul <saad.oulhaj@gmail.com>
2015-08-04 01:54:11 +0200

    tuto3

--- monsite/settings.py
+++ monsite/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'home'
+    'polls'
 )
 
 MIDDLEWARE_CLASSES = (
@@ -88,7 +88,7 @@ DATABASES = {
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Paris'
 
 USE_I18N = True
 
